04403b9635a3c4a5ba2b30c3fce8bbcd1c22c1fb
Adnan Roomieh <adnanroomieh@gmail.com>
2026-04-10 13:52:10 +0300

    feat: added shadcn/ui elements from reactbits.dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Silk from '@/components/Silk'
 
 function App() {
 
@@ -6,6 +7,13 @@ function App() {
     <>
       <div className="bg-blue-500 min-h-screen flex-col align-text-top flex items-center">
         Adnan Roomieh
+        <Silk
+          speed={5}
+          scale={1}
+          color="#0a0f9e"
+          noiseIntensity={0.8}
+          rotation={4.3}
+        />
       </div>
     </>
   )
